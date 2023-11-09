625f6f0fea6fea9aeea9d8b47b41bb389c504acf
RYU-Dae-Hee <eogml0716@gmail.com>
2023-11-09 17:09:46 +0900

    ADD : 서브 모듈에 Application.java 파일 추가
    실행 확인 완료

---
+++ community-interaction/src/main/java/daehee/challengehub/CommunityInteractionApplication.java
@@ -0,0 +1,13 @@
+package daehee.challengehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityInteractionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityInteractionApplication.class, args);
+    }
+
+}
