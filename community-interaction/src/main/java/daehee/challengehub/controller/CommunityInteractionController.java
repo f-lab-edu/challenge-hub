d561b10dcfbe9581e4a986c1ca003e07f7aba845
RYU-Dae-Hee <eogml0716@gmail.com>
2023-11-12 00:39:53 +0900

    DELETE : 무분별한 import 제거 (체리픽)

--- community-interaction/src/main/java/daehee/challengehub/controller/CommunityInteractionController.java
+++ community-interaction/src/main/java/daehee/challengehub/controller/CommunityInteractionController.java
@@ -2,8 +2,16 @@ package daehee.challengehub.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 import daehee.challengehub.service.CommunityInteractionService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class CommunityInteractionController {
