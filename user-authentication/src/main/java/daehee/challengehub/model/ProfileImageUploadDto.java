4051281aa59dd9b61e0829a23ad2ccd6059eb8ec
RYU-Dae-Hee <eogml0716@gmail.com>
2023-11-12 03:39:25 +0900

    ADD: 오류 해결을 위해서 일단 커밋

---
+++ user-authentication/src/main/java/daehee/challengehub/model/ProfileImageUploadDto.java
@@ -0,0 +1,8 @@
+package daehee.challengehub.model;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class ProfileImageUploadDto {
+    private MultipartFile imageFile;
+    // 생성자, Getter, Setter
+}
