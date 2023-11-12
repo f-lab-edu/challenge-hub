471b6eba2f5f49628ef36e687ab4a7d89cbd1a95
RYU-Dae-Hee <eogml0716@gmail.com>
2023-11-12 23:48:49 +0900

    UPDATE : 챌린지 관련 DTO 파일 수정

--- challenge-management/src/main/java/daehee/challengehub/model/ChallengeTagDto.java
+++ challenge-management/src/main/java/daehee/challengehub/model/ChallengeTagDto.java
@@ -1,6 +1,12 @@
 package daehee.challengehub.model;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class ChallengeTagDto {
-    private Long challengeId;
-    private String tag;
+    private final Long challengeId; // 챌린지 ID
+    private final String tag; // 태그 내용
+    private final String tagDescription; // 태그에 대한 설명
 }
