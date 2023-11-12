cb1531a199bcd24f8845bafe0e91e6e9c8d8e228
RYU-Dae-Hee <eogml0716@gmail.com>
2023-11-12 23:38:39 +0900

    ADD : UserController 작업 완료, 변경 사항 있을 시 수정 예정

--- user-authentication/src/main/java/daehee/challengehub/controller/UserController.java
+++ user-authentication/src/main/java/daehee/challengehub/controller/UserController.java
@@ -67,19 +67,31 @@ public class UserController {
 
     // 소셜 계정으로 회원가입
     @PostMapping("/users/social")
-    public ResponseEntity<String> signupWithSocial(@RequestBody UserSocialLoginDto userSocialLoginDto) {
+    public ResponseEntity<String> signupWithSocial() {
         // 임의의 소셜 계정 정보 생성
         UserSocialLoginDto newUser = UserSocialLoginDto.builder()
                 .provider("Google")
                 .token("sampleToken")
                 .build();
 
-        String responseMessage = String.format("소셜 계정(%s)으로 회원가입 성공. 토큰: %s",
-                newUser.getProvider(), newUser.getToken());
+        // TODO: 임의의 값을 내가 다시 넣어서 시나리오 완성 시키기
+        String responseMessage;
+        switch (newUser.getToken()) {
+            case "validToken":
+                responseMessage = String.format("소셜 계정(%s)으로 회원가입 성공. 토큰: %s",
+                        newUser.getProvider(), newUser.getToken());
+                break;
+            case "expiredToken":
+                responseMessage = "소셜 로그인 실패: 만료된 토큰";
+                break;
+            default:
+                responseMessage = "소셜 로그인 실패: 잘못된 토큰";
+        }
+
         return ResponseEntity.ok(responseMessage);
     }
 
-    // 일반 로그인
+
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody UserLoginDto userLoginDto) {
         // 임의의 로그인 데이터 생성
@@ -87,24 +99,39 @@ public class UserController {
                 .email("user@example.com")
                 .password("password123")
                 .rememberMe(true)
+                .twoFactorAuthCode("2FA123")
                 .build();
 
-        String responseMessage = "로그인 성공: " + loginUser.getEmail();
+        String responseMessage;
+        if ("user@example.com".equals(loginUser.getEmail()) && "password123".equals(loginUser.getPassword())) {
+            responseMessage = "로그인 성공: " + loginUser.getEmail();
+        } else {
+            responseMessage = "로그인 실패: 잘못된 이메일 또는 비밀번호";
+        }
+
         return ResponseEntity.ok(responseMessage);
     }
+
 
     // 소셜 로그인
     @PostMapping("/login/social")
-    public ResponseEntity<String> loginWithSocial(@RequestBody UserLoginDto userLoginDto) {
+    public ResponseEntity<String> loginWithSocial(@RequestBody UserSocialLoginDto userSocialLoginDto) {
         // 임의의 소셜 로그인 데이터 생성
-        UserLoginDto socialLoginUser = UserLoginDto.builder()
-                .email("socialuser@example.com")
-                .rememberMe(false)
+        UserSocialLoginDto socialLoginUser = UserSocialLoginDto.builder()
+                .provider("Facebook")
+                .token("sampleTokenFacebook123")
                 .build();
 
-        String responseMessage = "소셜 로그인 성공: " + socialLoginUser.getEmail();
+        String responseMessage;
+        if ("Facebook".equals(socialLoginUser.getProvider()) && "sampleTokenFacebook123".equals(socialLoginUser.getToken())) {
+            responseMessage = "소셜 로그인 성공: 프로바이더 - " + socialLoginUser.getProvider();
+        } else {
+            responseMessage = "소셜 로그인 실패: 잘못된 프로바이더 또는 토큰";
+        }
+
         return ResponseEntity.ok(responseMessage);
     }
+
 
     // 2단계 인증
     @PostMapping("/login/2fa")
@@ -112,39 +139,64 @@ public class UserController {
         // 임의의 2단계 인증 데이터 생성
         UserLoginDto twoFaUser = UserLoginDto.builder()
                 .email("2fauser@example.com")
-                .twoFactorAuthCode("123456")
+                .twoFactorAuthCode("2FA123456")
                 .build();
 
-        String responseMessage = "2단계 인증 성공: " + twoFaUser.getEmail();
+        String responseMessage;
+        if ("2FA123456".equals(twoFaUser.getTwoFactorAuthCode())) {
+            responseMessage = "2단계 인증 성공: 이메일 - " + twoFaUser.getEmail();
+        } else {
+            responseMessage = "2단계 인증 실패: 잘못된 인증 코드";
+        }
+
         return ResponseEntity.ok(responseMessage);
     }
+
 
     // 비밀번호 재설정
     @PostMapping("/password/reset")
     public ResponseEntity<String> resetPassword(@RequestBody PasswordChangeDto passwordChangeDto) {
-        // 임의의 비밀번호 재설정 데이터 생성
-        PasswordChangeDto resetPasswordData = PasswordChangeDto.builder()
+        // 성공적인 비밀번호 변경 시나리오
+        PasswordChangeDto successfulChange = PasswordChangeDto.builder()
                 .currentPassword("currentPassword123")
                 .newPassword("newPassword456")
                 .build();
 
-        String responseMessage = "비밀번호 재설정 성공: " + resetPasswordData.getNewPassword();
-        return ResponseEntity.ok(responseMessage);
+        String successMessage = "비밀번호 재설정 성공: 새 비밀번호 - " + successfulChange.getNewPassword();
+
+        // 실패한 비밀번호 변경 시나리오 (예: 현재 비밀번호 불일치)
+        PasswordChangeDto failedChange = PasswordChangeDto.builder()
+                .currentPassword("wrongCurrentPassword")
+                .newPassword("newPassword789")
+                .build();
+
+        String failureMessage = "비밀번호 재설정 실패: 현재 비밀번호 불일치";
+
+        // 임의로 성공 또는 실패 시나리오 선택
+        boolean isSuccessful = true; // 이 값을 변경하여 성공/실패 시나리오 선택
+        return ResponseEntity.ok(isSuccessful ? successMessage : failureMessage);
     }
+
 
     // 프로필 조회
     @GetMapping("/profile")
-    public ResponseEntity<UserProfileDto> getProfile() {
-        // 임의의 유저 프로필 데이터 생성
-        UserProfileDto userProfile = UserProfileDto.builder()
+    public ResponseEntity<?> getProfile() {
+        // 성공적인 프로필 조회 시나리오
+        UserProfileDto successfulProfile = UserProfileDto.builder()
                 .username("sampleUser")
                 .email("user@example.com")
                 .bio("Sample bio")
                 .location("Sample location")
                 .build();
 
-        return ResponseEntity.ok(userProfile);
+        // 실패한 프로필 조회 시나리오 (예: 프로필이 존재하지 않음)
+        String failureMessage = "프로필 조회 실패: 사용자를 찾을 수 없습니다.";
+
+        // 임의로 성공 또는 실패 시나리오 선택
+        boolean isSuccessful = true; // 이 값을 변경하여 성공/실패 시나리오 선택
+        return isSuccessful ? ResponseEntity.ok(successfulProfile) : ResponseEntity.badRequest().body(failureMessage);
     }
+
 
     // 프로필 업데이트
     @PutMapping("/profile")
@@ -192,7 +244,14 @@ public class UserController {
     // 성과 목록 조회
     @GetMapping("/profile/achievements")
     public ResponseEntity<List<String>> getAchievements() {
-        List<String> achievements = Arrays.asList("Achievement 1", "Achievement 2", "Achievement 3");
+        // 임의의 성과 목록 생성
+        List<String> achievements = Arrays.asList(
+                "10일 연속 챌린지 완료",
+                "커뮤니티에서 활발한 활동",
+                "첫 챌린지 성공",
+                "최다 좋아요 획득"
+        );
         return ResponseEntity.ok(achievements);
     }
+
 }
