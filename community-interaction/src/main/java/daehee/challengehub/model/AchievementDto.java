ddeb527a2fc65c294b650afd587bd5dba4cbdf39
RYU-Dae-Hee <eogml0716@gmail.com>
2023-11-14 04:32:24 +0900

    DELETE : CommunityInteraction 관련 Controller, DTO에서 필요없는 필드들 삭제 및 변경

--- community-interaction/src/main/java/daehee/challengehub/model/AchievementDto.java
+++ community-interaction/src/main/java/daehee/challengehub/model/AchievementDto.java
@@ -10,5 +10,4 @@ public class AchievementDto {
     private final Long challengeId; // 챌린지 ID
     private final String achievementDetails; // 성취 내용
     private final String achievedDate; // 성취한 날짜
-    private final boolean isVerified; // 성취 인증 여부
 }
