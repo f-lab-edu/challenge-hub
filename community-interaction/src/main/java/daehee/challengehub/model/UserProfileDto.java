dfa16bce8514e65c5b28d3d22b87135763d9695d
RYU-Dae-Hee <eogml0716@gmail.com>
2023-11-14 04:21:11 +0900

    DELETE : CommunityInteraction 관련 Controller, DTO에서 필요없는 필드들 삭제 및 변경
    (cherry picked from commit 66b8f39b2e6ba00d2e76880b27cf3b1332928762)

--- community-interaction/src/main/java/daehee/challengehub/model/UserProfileDto.java
+++ community-interaction/src/main/java/daehee/challengehub/model/UserProfileDto.java
@@ -10,5 +10,4 @@ public class UserProfileDto {
     private final String email;
     // TODO: 유저 프로필로 받을 다른 정보들에 대해서 더 생각해볼 것
     private final String bio;       // 사용자 소개
-    private final String location;  // 사용자 위치
 }
