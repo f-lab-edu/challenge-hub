66b8f39b2e6ba00d2e76880b27cf3b1332928762
RYU-Dae-Hee <eogml0716@gmail.com>
2023-11-14 04:21:11 +0900

    DELETE : CommunityInteraction 관련 Controller, DTO에서 필요없는 필드들 삭제 및 변경

--- community-interaction/src/main/java/daehee/challengehub/controller/CommunityInteractionController.java
+++ community-interaction/src/main/java/daehee/challengehub/controller/CommunityInteractionController.java
@@ -1,16 +1,12 @@
 package daehee.challengehub.controller;
 
 import daehee.challengehub.model.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Arrays;
 import java.util.List;
@@ -91,38 +87,6 @@ public class CommunityInteractionController {
         return ResponseEntity.ok(followersList);
     }
 
-
-    // 챌린지 그룹 생성
-    @PostMapping("/groups")
-    public ResponseEntity<String> createGroup(@RequestBody GroupDto groupDto) {
-        GroupDto newGroup = GroupDto.builder()
-                .groupId(1L)
-                .groupName("챌린지 그룹123")
-                .groupDescription("새로운 챌린지 그룹 설명")
-                .creatorId(123L)
-                .creationDate("2023-11-15")
-                .build();
-
-        String responseMessage = String.format("그룹 '%s' 생성 성공", newGroup.getGroupName());
-        return ResponseEntity.ok(responseMessage);
-    }
-
-
-    // 특정 그룹 상세 조회
-    @GetMapping("/groups/{id}")
-    public ResponseEntity<GroupDto> getGroupDetails(@PathVariable Long id) {
-        GroupDto groupDetails = GroupDto.builder()
-                .groupId(id)
-                .groupName("샘플 그룹")
-                .groupDescription("이것은 샘플 그룹에 대한 설명입니다.")
-                .creatorId(123L)
-                .creationDate("2023-11-15")
-                .build();
-
-        return ResponseEntity.ok(groupDetails);
-    }
-
-
     // 그룹 메시지 전송
     @PostMapping("/groups/{id}/messages")
     public ResponseEntity<String> sendMessageToGroup(@PathVariable Long id, @RequestBody MessageDto messageDto) {
@@ -163,7 +127,6 @@ public class CommunityInteractionController {
     }
 
 
-
     // 다른 사용자의 프로필 조회
     @GetMapping("/user/{userId}/profile")
     public ResponseEntity<UserProfileDto> getUserProfile(@PathVariable Long userId) {
@@ -171,7 +134,6 @@ public class CommunityInteractionController {
                 .username("user123")
                 .email("user123@example.com")
                 .bio("이곳에 사용자 바이오")
-                .location("서울")
                 .build();
 
         return ResponseEntity.ok(userProfile);
@@ -216,71 +178,6 @@ public class CommunityInteractionController {
 
         return ResponseEntity.ok(messagesList);
     }
-
-
-
-    // 그룹 정보 수정
-    @PutMapping("/groups/{id}")
-    public ResponseEntity<String> updateGroup(@PathVariable Long id, @RequestBody GroupDto groupDto) {
-        GroupDto updatedGroup = GroupDto.builder()
-                .groupId(1L)
-                .groupName("새로운 그룹 이름")
-                .groupDescription("새로운 그룹 설명")
-                .creatorId(123L)
-                .creationDate("2023-01-01")
-                .build();
-
-        String responseMessage = String.format("그룹 ID %d 업데이트 성공: %s, %s",
-                id, updatedGroup.getGroupName(), updatedGroup.getGroupDescription());
-        return ResponseEntity.ok(responseMessage);
-    }
-
-
-    // 그룹 삭제
-    @DeleteMapping("/groups/{id}")
-    public ResponseEntity<String> deleteGroup(@PathVariable Long id) {
-        GroupDto groupToDelete = GroupDto.builder()
-                .groupId(1L)
-                .build();
-
-        String responseMessage = String.format("그룹 ID %d 삭제 성공", groupToDelete.getGroupId());
-        return ResponseEntity.ok(responseMessage);
-    }
-
-
-    // 그룹에 멤버 추가
-    // TODO: 그룹에 멤버 구현하는 로직 자체가 완성이 덜 되었는데 분리 시키는 거 고려해보기
-    @PostMapping("/groups/{id}/members/{userId}")
-    public ResponseEntity<String> addMemberToGroup(@PathVariable Long groupId, @PathVariable Long userId) {
-        // 임의의 그룹과 멤버 정보 생성
-        GroupDto group = GroupDto.builder()
-                .groupId(groupId)
-                .groupName("챌린지 그룹")
-                .groupDescription("이 그룹은 챌린지 참가자들을 위한 공간입니다.")
-                .creatorId(123L)
-                .creationDate("2023-01-01")
-                .build();
-
-        String responseMessage = String.format("멤버 ID %d가 그룹 ID %d에 추가되었습니다.", userId, group.getGroupId());
-        return ResponseEntity.ok(responseMessage);
-    }
-
-
-    // TODO: GroupMemberRemovalDto 구현해야할 듯, GroupMemberRemovalDto 관련 컨트롤러도 분리가 필요해보임
-    // 그룹에서 멤버 제거
-//    @DeleteMapping("/groups/{id}/members/{userId}")
-//    public ResponseEntity<String> removeMemberFromGroup(@PathVariable Long groupId, @PathVariable Long userId) {
-//        // 임의의 그룹 멤버 제거 결과 데이터 생성
-//        GroupMemberRemovalDto removalResult = GroupMemberRemovalDto.builder()
-//                .groupId(groupId) // 그룹 ID
-//                .userId(userId) // 제거된 멤버의 사용자 ID
-//                .removalDate("2023-11-10") // 멤버 제거 날짜
-//                .wasSuccessful(true) // 제거 성공 여부
-//                .build();
-//
-//        String responseMessage = String.format("그룹 ID %d에서 사용자 ID %d의 멤버 제거 성공", groupId, userId);
-//        return ResponseEntity.ok(responseMessage);
-//    }
 
     // TODO: FriendRequestDto 구현해야할 듯, Friend 관련 컨트롤러도 분리가 필요해보임
     // 친구 요청 목록 조회
@@ -341,29 +238,6 @@ public class CommunityInteractionController {
 
         return ResponseEntity.ok(communityFeed);
     }
-
-
-    // TODO: 제거 예정
-//    // 그룹 이벤트 생성
-//    @PostMapping("/groups/{id}/events")
-//    public ResponseEntity<String> createGroupEvent(@PathVariable Long id, @RequestBody String eventData) {
-//        return ResponseEntity.ok(communityService.createGroupEvent(id, eventData));
-//    }
-
-    // TODO: 제거 예정
-//    // 그룹 이벤트 목록 조회
-//    @GetMapping("/groups/{id}/events")
-//    public ResponseEntity<String> getGroupEvents(@PathVariable Long id) {
-//        return ResponseEntity.ok(communityService.getGroupEvents(id));
-//    }
-
-
-    // TODO: 제거 예정
-//    // 이벤트 참여
-//    @PostMapping("/groups/{id}/events/{eventId}/participate")
-//    public ResponseEntity<String> participateInEvent(@PathVariable Long id, @PathVariable Long eventId) {
-//        return ResponseEntity.ok(communityService.participateInEvent(eventId));
-//    }
 
 
     // 커뮤니티 포스트 작성
@@ -434,14 +308,6 @@ public class CommunityInteractionController {
         String responseMessage = String.format("포스트 ID %d에 좋아요 성공", postId);
         return ResponseEntity.ok(responseMessage);
     }
-
-
-    // TODO: 제거 예정
-//    // 오프라인 또는 온라인 모임, 이벤트 생성 및 관리
-//    @PostMapping("/community/events")
-//    public ResponseEntity<String> createCommunityEvent(@RequestBody String eventDetails) {
-//        return ResponseEntity.ok(communityService.createCommunityEvent(eventDetails));
-//    }
 
 
     // 사용자들의 챌린지나 활동에서의 성취 공유
--- community-interaction/src/main/java/daehee/challengehub/model/MessageDto.java
+++ community-interaction/src/main/java/daehee/challengehub/model/MessageDto.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @Builder
 public class MessageDto {
     private final Long senderId; // 메시지 발신자 ID
-    private final Long receiverId; // 메시지 수신자 ID, 이것은 그룹 ID 또는 개인 사용자 ID가 될 수 있습니다.
+    private final Long receiverId; // 메시지 수신자 ID
     private final String messageContent; // 메시지 내용
     private final String sentTime; // 메시지 보낸 시간
     private final boolean isRead; // 메시지 읽음 여부
