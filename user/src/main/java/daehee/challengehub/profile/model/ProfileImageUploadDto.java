04e67097b731dff4a5e89a5a0e01e2741d1fa822
RYU-Dae-Hee <eogml0716@gmail.com>
2023-11-14 20:31:13 +0900

    DELETE : UserController.java 삭제

--- user/src/main/java/daehee/challengehub/profile/model/ProfileImageUploadDto.java
+++ user/src/main/java/daehee/challengehub/profile/model/ProfileImageUploadDto.java
@@ -7,5 +7,5 @@ import org.springframework.web.multipart.MultipartFile;
 @Getter
 @Builder
 public class ProfileImageUploadDto {
-    private final MultipartFile imageFile;
+    private final MultipartFile imageFile; // TODO: 이것도 그냥 ImageUrl로 변경 시킬까 고민되는데 더 알아보고 바꿔보자
 }
