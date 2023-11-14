f3ce257131d567c6d9d2b3797d23edd3d9805719
RYU-Dae-Hee <eogml0716@gmail.com>
2023-11-14 20:31:13 +0900

    DELETE : UserController.java 삭제 (체리픽)

--- user/src/main/java/daehee/challengehub/profile/model/ProfileImageUploadDto.java
+++ user/src/main/java/daehee/challengehub/profile/model/ProfileImageUploadDto.java
@@ -4,9 +4,13 @@ import org.springframework.web.multipart.MultipartFile;
 
 public class ProfileImageUploadDto {
 <<<<<<< HEAD
+<<<<<<< HEAD
     private MultipartFile imageFile;
     // 생성자, Getter, Setter
 =======
     private final MultipartFile imageFile;
 >>>>>>> fa7a123 (DELETE : User관련 DTO에서 필요없는 필드들 삭제)
+=======
+    private final MultipartFile imageFile; // TODO: 이것도 그냥 ImageUrl로 변경 시킬까 고민되는데 더 알아보고 바꿔보자
+>>>>>>> 04e6709 (DELETE : UserController.java 삭제)
 }
