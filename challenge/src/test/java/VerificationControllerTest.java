a4c0281f8d0df8c4c4746abaff1f97363f055e7a
RYU-Dae-Hee <eogml0716@gmail.com>
2023-11-19 08:32:06 +0900

    ADD : Challenge 관련 TEST 코드 작성

---
+++ challenge/src/test/java/VerificationControllerTest.java
@@ -0,0 +1,83 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import daehee.challengehub.ChallengeApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = ChallengeApplication.class)
+@AutoConfigureMockMvc
+public class VerificationControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    // 챌린지 인증 업로드 테스트
+    @Test
+    public void testUploadVerification() throws Exception {
+        String verificationJson = "{\"verificationText\":\"이번 주 도전 성공!\"}";
+        MvcResult result = mockMvc.perform(post("/challenges/3/verification")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(verificationJson))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Map<String, Object> response = objectMapper.readValue(result.getResponse().getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {});
+        assertEquals("이번 주 도전 성공!", ((Map) response.get("newVerification")).get("verificationText"));
+    }
+
+    // 챌린지 인증 내역 조회 테스트
+    @Test
+    public void testGetVerifications() throws Exception {
+        MvcResult result = mockMvc.perform(get("/challenges/3/verification"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Map<String, Object> response = objectMapper.readValue(result.getResponse().getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {});
+        assertTrue(((List) response.get("verifications")).size() == 2);
+    }
+
+    // 챌린지 인증 수정 테스트
+    @Test
+    public void testUpdateVerification() throws Exception {
+        String updatedVerificationJson = "{\"verificationText\":\"챌린지 인증 수정됨\"}";
+        MvcResult result = mockMvc.perform(put("/challenges/3/verification/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(updatedVerificationJson))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Map<String, Object> response = objectMapper.readValue(result.getResponse().getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {});
+        assertEquals("챌린지 인증 수정됨", ((Map) response.get("updatedVerification")).get("verificationText"));
+    }
+
+    // 챌린지 인증 삭제 테스트
+    @Test
+    public void testDeleteVerification() throws Exception {
+        MvcResult result = mockMvc.perform(delete("/challenges/3/verification/1"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Map<String, String> response = objectMapper.readValue(result.getResponse().getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {});
+        assertTrue(response.get("message").contains("챌린지 인증 삭제 성공: 인증 ID 1"));
+    }
+}
