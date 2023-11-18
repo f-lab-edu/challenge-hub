5b9f5746814b64197a86245a784942b67d57e5c4
RYU-Dae-Hee <eogml0716@gmail.com>
2023-11-19 07:38:21 +0900

    UPDATE : CommunityControllerTest.java 코드 수정

--- social/src/test/java/CommunityControllerTest.java
+++ social/src/test/java/CommunityControllerTest.java
@@ -1,3 +1,5 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import daehee.challengehub.SocialApplication;
 
 import org.junit.jupiter.api.Test;
@@ -6,16 +8,18 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @SpringBootTest(classes = SocialApplication.class)
 @AutoConfigureMockMvc
@@ -24,54 +28,75 @@ public class CommunityControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
     public void testGetCommunityFeed() throws Exception {
-        mockMvc.perform(get("/community/feed"))
+        MvcResult result = mockMvc.perform(get("/community/feed"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].postTitle", is("첫 번째 커뮤니티 포스트")))
-                .andExpect(jsonPath("$[1].postTitle", is("두 번째 커뮤니티 포스트")));
+                .andReturn();
+
+        Map<String, Object> response = objectMapper.readValue(result.getResponse().getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {});
+        assertNotNull(response.get("communityFeed"));
+        assertTrue(((List<?>) response.get("communityFeed")).size() > 0);
     }
 
     @Test
     public void testCreateCommunityPost() throws Exception {
         String postJson = "{\"authorId\":101,\"postContent\":\"새로운 포스트 내용\",\"postTitle\":\"새로운 포스트\"}";
-        mockMvc.perform(post("/community/posts")
+        MvcResult result = mockMvc.perform(post("/community/posts")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(postJson))
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("커뮤니티 포스트 생성 성공: 새로운 포스트")));
+                .andReturn();
+
+        Map<String, Object> response = objectMapper.readValue(result.getResponse().getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {});
+        assertEquals("새로운 포스트", ((Map<?, ?>)response.get("createdPost")).get("postTitle"));
     }
 
     @Test
     public void testUpdateCommunityPost() throws Exception {
         String updatedPostJson = "{\"authorId\":101,\"postContent\":\"수정된 포스트 내용\",\"postTitle\":\"수정된 포스트\"}";
-        mockMvc.perform(put("/community/posts/1")
+        MvcResult result = mockMvc.perform(put("/community/posts/1")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(updatedPostJson))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.postTitle", is("수정된 포스트")))
-                .andExpect(jsonPath("$.postContent", is("수정된 포스트 내용")));
+                .andReturn();
+
+        Map<String, Object> response = objectMapper.readValue(result.getResponse().getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {});
+        assertEquals("수정된 포스트", ((Map<?, ?>)response.get("updatedPost")).get("postTitle"));
     }
 
     @Test
     public void testDeleteCommunityPost() throws Exception {
-        mockMvc.perform(delete("/community/posts/1"))
+        MvcResult result = mockMvc.perform(delete("/community/posts/3"))
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("포스트 삭제 성공: 포스트 ID 3")));
+                .andReturn();
+
+        Map<String, String> response = objectMapper.readValue(result.getResponse().getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {});
+        assertEquals("포스트 삭제 성공: 포스트 ID 3", response.get("message"));
     }
 
     @Test
     public void testGetCommunityPosts() throws Exception {
-        mockMvc.perform(get("/community/posts"))
+        MvcResult result = mockMvc.perform(get("/community/posts"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].postTitle", is("첫 번째 포스트")))
-                .andExpect(jsonPath("$[1].postTitle", is("두 번째 포스트")));
+                .andReturn();
+
+        Map<String, Object> response = objectMapper.readValue(result.getResponse().getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {});
+        assertNotNull(response.get("posts"));
+        assertTrue(((List<?>) response.get("posts")).size() > 0);
     }
 
     @Test
     public void testLikeCommunityPost() throws Exception {
-        mockMvc.perform(post("/community/posts/1/like"))
+        MvcResult result = mockMvc.perform(post("/community/posts/1/like"))
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("포스트 ID 1에 좋아요 성공")));
+                .andReturn();
+
+        Map<String, Object> response = objectMapper.readValue(result.getResponse().getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {});
+        assertEquals(String.format("포스트 ID %d에 좋아요 성공", 1), response.get("message"));
+        assertNotNull(response.get("likedPost"));
     }
 }
