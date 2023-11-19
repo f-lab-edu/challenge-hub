0d482fc46c5fa2715b16feb033d6ca97ccbca68e
RYU-Dae-Hee <eogml0716@gmail.com>
2023-11-20 00:31:42 +0900

    ADD : ExceptionHandler 관련 작업
    모듈 통합하고 이거 좀 합쳐야될 거 같다.

---
+++ challenge/src/main/java/daehee/challengehub/exception/CustomException.java
@@ -0,0 +1,14 @@
+package daehee.challengehub.exception;
+
+import daehee.challengehub.constants.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
