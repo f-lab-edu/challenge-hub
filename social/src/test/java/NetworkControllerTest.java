44fef3d973f61392cc8f250efef5e25774d8022e
Dae-Hee-Ryu <57823242+eogml0716@users.noreply.github.com>
2023-11-25 17:07:38 +0900

    Social (Controller, DTO, Test) (#6)
    * Initial commit
    
    * 정리: .gitignore 파일
    
    불필요한 IDE 설정 제거하였습니다.
    
    * 정리: .gitignore 파일
    
    불필요한 IDE 설정 제거 중
    
    * 정리: build.gradle
    
    ext set('snippetsDir', ...)은 문서 생성 시 사용되는 코드 스니펫을 저장할 디렉토리 경로인데 당장은 사용하지 않을 거 같아서 제거
    
    * 정리: build.gradle
    
    완전 초창기, 기획만 했을 때라고 생각하고 build.gradle을 내용 삭제
    
    * 정리: build.gradle
    
    Junit5 5.10.0 코드 추가
    
    * 정리: build.gradle
    
    actuator 일단 주석 처리
    
    * 정리: build.gradle
    
    일단 test 관련 부분 빼곤 전부 주석 처리
    
    dependencies를 다른 걸 쓸 수도 있는데 다른 사람들이 많이 쓴다고 해서 일단 넣어두는 행동은 별로인 거 같은데
    
    * 정리: build.gradle
    
    주석 제거
    
    * 추가 : settings.gradle, build.gradle
    
    공부 더 해야할 거 주석 추가
    
    * 추가 : settings.gradle
    
    :가 있는 거랑 :가 없는 거랑 무슨 차이지
    
    * 추가 : sourceCompatibility, targetCompatiblity 관련
    
    * 정리 : settings.gradle, build.gradle 주석 제거
    
    * 추가 : build.gradle
    
    Gradle 8.4에서는 toolchain 사용을 권장
    
    * 추가 : 서브 모듈의 build.gradle에 스프링 부트 웹 실행을 위한 디펜던시
    
    * DELETE : 루트 build.gradle 삭제
    
    * ADD : 서브 모듈에 Application.java 파일 추가
    
    실행 확인 완료
    
    * ADD : 서브 모듈에 Application.java 파일 추가
    
    실행 확인 완료
    
    * DELETE : 루트 src 파일 삭제
    
    * DELETE : 툴체인 삭제
    
    * UPDATE : testImplementation 수정, EOF 처리
    
    * DELETE : settings.gradle root 네임 삭제
    
    * DELETE : settings.gradle root 네임 삭제
    
    * ADD : Controller 스켈레톤 코드 작성
    
    * Revert "ADD : Controller 스켈레톤 코드 작성"
    
    This reverts commit 6315f919ef5ca0619cf1f7849be3fa3f09ff4e6e.
    
    * DELETE : junit 버전 삭제
    
    * ADD : 커뮤니티 관련 Controller 파일 추가 중, 변경 예정 (체리픽)
    
    * ADD : 커뮤니티 관련 Controller 파일 추가 중, 변경 예정
    
    (cherry picked from commit 5ce716e4b0139bbf6876983cbd742eecda78f4f0)
    
    * DELETE : CommunityInteraction 관련 Controller, DTO에서 필요없는 필드들 삭제 및 변경
    
    (cherry picked from commit 66b8f39b2e6ba00d2e76880b27cf3b1332928762)
    
    * DELETE : CommunityInteraction 관련 Controller, DTO에서 필요없는 필드들 삭제 및 변경
    
    (cherry picked from commit ddeb527a2fc65c294b650afd587bd5dba4cbdf39)
    
    * DELETE : CommunityInteraction 관련 Controller 삭제 (체리픽)
    
    * ADD: CommunityController.java 작성 완료 (체리픽)
    
    * ADD: NetworkController.java 작성 완료 (체리픽)
    
    * UPDATE : 서브 모듈에서 Social쪽 컨트롤러 코드 수정 (체리픽)
    
    * ADD : Network 테스트 코드 작성 중 (체리픽)
    
    * ADD : Community 테스트 코드 작성 완료
    
    (cherry picked from commit 9d0e38488763cb9c2b60b2960b75f4ab0a692d17)
    
    * DELETE: Social 관련 파일 빼고 전부 삭제
    
    * UPDATE: 체리픽 할 때 충돌 난 거 해결
    
    * ADD :build.gradle이 누락되어서 추가
    
    * ADD:SocialApplication이 누락되어서 추가
    
    * DELETE : ResponseEntity 관련 코드 삭제
    
    * UPDATE : CommunityController.java, NetworkController.java의 return 값으로 변경
    
    * UPDATE : CommunityControllerTest.java 코드 수정
    
    * ADD : NetworkControllerTest.java 코드 추가
    
    * ADD : ExceptionHandler 관련 작업
    
    모듈 통합하고 이거 좀 합쳐야될 거 같다.
    
    * ADD, UPDATE : JsonBuilder.java 작성해서 Test 코드 변경

---
+++ social/src/test/java/NetworkControllerTest.java
@@ -0,0 +1,73 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import daehee.challengehub.SocialApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = SocialApplication.class)
+@AutoConfigureMockMvc
+public class NetworkControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void testFollowUser() throws Exception {
+        MvcResult result = mockMvc.perform(post("/network/follow/456"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Map<String, Object> response = objectMapper.readValue(result.getResponse().getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {});
+        assertEquals(String.format("사용자 %d 팔로우 성공. 상호 팔로우 상태: 예", 456), response.get("message"));
+        assertNotNull(response.get("followDetails"));
+    }
+
+    @Test
+    public void testGetFollowings() throws Exception {
+        MvcResult result = mockMvc.perform(get("/network/following"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Map<String, Object> response = objectMapper.readValue(result.getResponse().getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {});
+        assertNotNull(response.get("followingList"));
+        assertFalse(((List<?>) response.get("followingList")).isEmpty());
+    }
+
+    @Test
+    public void testUnfollowUser() throws Exception {
+        MvcResult result = mockMvc.perform(delete("/network/follow/1"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Map<String, String> response = objectMapper.readValue(result.getResponse().getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {});
+        assertEquals("사용자 ID 1 언팔로우 성공", response.get("message"));
+    }
+
+    @Test
+    public void testGetFollowers() throws Exception {
+        MvcResult result = mockMvc.perform(get("/network/followers"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Map<String, Object> response = objectMapper.readValue(result.getResponse().getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {});
+        assertNotNull(response.get("followers"));
+        assertFalse(((List<?>) response.get("followers")).isEmpty());
+    }
+}
