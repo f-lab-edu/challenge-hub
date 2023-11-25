209f98190b222f579511a40a3f3fef3dd6e8ced2
Dae-Hee-Ryu <57823242+eogml0716@users.noreply.github.com>
2023-11-25 17:07:51 +0900

    Challenge (Controller, DTO, Test) (#5)
    * Initial commit
    
    * 정리: .gitignore 파일
    
    불필요한 IDE 설정 제거하였습니다.
    
    * 정리: .gitignore 파일
    
    불필요한 IDE 설정 제거 중
    
    * 정리: build.gradle
    
    ext set('snippetsDir', ...)은 문서 생성 시 사용되는 코드 스니펫을 저장할 디렉토리 경로인데 당장은 사용하지 않을 거 같아서 제거
    
    * 정리: build.gradle
    
    완전 초창기, 기획만 했을 때라고 생각하고 build.gradle을 내용 삭제
    
    * 정리: build.gradle
    
    Junit5 5.10.0 코드 추가
    
    * 정리: build.gradle
    
    actuator 일단 주석 처리
    
    * 정리: build.gradle
    
    일단 test 관련 부분 빼곤 전부 주석 처리
    
    dependencies를 다른 걸 쓸 수도 있는데 다른 사람들이 많이 쓴다고 해서 일단 넣어두는 행동은 별로인 거 같은데
    
    * 정리: build.gradle
    
    주석 제거
    
    * 추가 : settings.gradle, build.gradle
    
    공부 더 해야할 거 주석 추가
    
    * 추가 : settings.gradle
    
    :가 있는 거랑 :가 없는 거랑 무슨 차이지
    
    * 추가 : sourceCompatibility, targetCompatiblity 관련
    
    * 정리 : settings.gradle, build.gradle 주석 제거
    
    * 추가 : build.gradle
    
    Gradle 8.4에서는 toolchain 사용을 권장
    
    * 추가 : 서브 모듈의 build.gradle에 스프링 부트 웹 실행을 위한 디펜던시
    
    * DELETE : 루트 build.gradle 삭제
    
    * ADD : 서브 모듈에 Application.java 파일 추가
    
    실행 확인 완료
    
    * ADD : 서브 모듈에 Application.java 파일 추가
    
    실행 확인 완료
    
    * DELETE : 루트 src 파일 삭제
    
    * DELETE : 툴체인 삭제
    
    * UPDATE : testImplementation 수정, EOF 처리
    
    * DELETE : settings.gradle root 네임 삭제
    
    * DELETE : settings.gradle root 네임 삭제
    
    * ADD : Controller 스켈레톤 코드 작성
    
    * Revert "ADD : Controller 스켈레톤 코드 작성"
    
    This reverts commit 6315f919ef5ca0619cf1f7849be3fa3f09ff4e6e.
    
    * DELETE : junit 버전 삭제
    
    * UPDATE : 챌린지 관련 DTO 파일 수정 (체리픽)
    
    * UPDATE : 챌린지 관련 Controller 파일 작업 중 (체리픽)
    
    * ADD : 챌린지 관련 Controller 파일 추가 중, 변경 예정
    
    (cherry picked from commit a0ba967b4900b8d850104e024d845297f1d5f7b2)
    
    * DELETE : Challenge 관련 Controller, DTO에서 필요없는 필드들 삭제 및 변경
    
    (cherry picked from commit 77aebc25140544e69c960cb49bcee6156316754a)
    
    * ADD: InteractionController.java 작성 완료 (체리픽)
    
    * ADD: ManagementController.java 작성 완료 (체리픽)
    
    * ADD: VerificationController.java 작성 완료
    
    (cherry picked from commit 1b24222cb880064e6c6c8cc32ce698a489738c2f)
    
    * UPDATE : 서브 모듈에서 Challenge쪽 컨트롤러 코드 수정
    
    (cherry picked from commit 0a65a528589076e4e23076df089babcecc9d514f)
    
    * DELETE : Challenge 관련 파일 남기고 전부 삭제
    
    * ADD :build.gradle이 누락되어서 추가
    
    * ADD:ChallengeApplication이 누락되어서 추가
    
    * DELETE : ResponseEntity 관련 코드 제거
    
    * UPDATE : Challenge 관련 Controller의 return 값들 업데이트
    
    * ADD : Challenge 관련 TEST 코드 작성
    
    * ADD : ExceptionHandler 관련 작업
    
    모듈 통합하고 이거 좀 합쳐야될 거 같다.
    
    * ADD, UPDATE : JsonBuilder.java 작성해서 Test 코드 변경

---
+++ challenge/src/main/java/daehee/challengehub/management/model/ChallengeDto.java
@@ -0,0 +1,21 @@
+package daehee.challengehub.management.model;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class ChallengeDto {
+    private final Long challengeId;
+    private final String title;
+    private final String description;
+    private final List<String> tags;
+    private final List<String> imageUrls;
+    private final String startDate;
+    private final String endDate;
+    private final String createdBy;
+    private final String createdAt;
+    private final String lastModified;
+}
