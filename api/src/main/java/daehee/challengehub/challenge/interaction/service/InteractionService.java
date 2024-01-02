170d3a7dd634b79b5869e0c6cb5dee8526e4c622
RYU-Dae-Hee <eogml0716@gmail.com>
2024-01-03 05:14:21 +0900

    DELETE : Interaction 모듈에서 알림 서비스 추가중인데 Repository 관련된 부분 해결 후 추가 예정

--- api/src/main/java/daehee/challengehub/challenge/interaction/service/InteractionService.java
+++ api/src/main/java/daehee/challengehub/challenge/interaction/service/InteractionService.java
@@ -1,12 +1,10 @@
 package daehee.challengehub.challenge.interaction.service;
 
 import daehee.challengehub.challenge.interaction.entity.ChatMessage;
-import daehee.challengehub.challenge.interaction.entity.ChatRoom;
 import daehee.challengehub.challenge.interaction.entity.Review;
 import daehee.challengehub.challenge.interaction.model.ChatMessageDto;
 import daehee.challengehub.challenge.interaction.model.ReviewDto;
 import daehee.challengehub.challenge.interaction.repository.InteractionRepository;
-import daehee.challengehub.kafka.model.InteractionNotificationMessage;
 import daehee.challengehub.kafka.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -28,25 +26,7 @@ public class InteractionService {
     // 챌린지 채팅방의 메시지를 전송하는 로직
     public ChatMessage postChatMessage(String challengeId, ChatMessageDto chatMessageDto) {
         ChatMessage chatMessage = interactionRepository.saveChatMessage(challengeId, chatMessageDto);
-        sendChatNotification(chatMessage);
         return chatMessage;
-    }
-
-
-    // 챌린지 채팅 메시지에 대한 알림을 보내는 로직
-    // TODO: 임시 코드 수정 예정, return값 void로 한 거 고치기
-    private void sendChatNotification(ChatMessage chatMessage) {
-        ChatRoom chatRoom = interactionRepository.findChatRoomById(chatMessage.getChatRoomId());
-        User sender = userRepository.findById(chatMessage.getSenderId());
-        Challenge challenge = challengeRepository.findById(chatRoom.getChallengeId());
-
-        InteractionNotificationMessage notificationMessage = InteractionNotificationMessage.builder()
-                .userIds(chatRoom.getParticipants())
-                .message(sender.getUsername() + "님이 '" + chatMessage.getContent() + "' - 챌린지 " + challenge.getTitle())
-                .chatMessage(chatMessage.getContent())
-                .challengeName(challenge.getTitle())
-                .build();
-        notificationService.sendNotification(notificationMessage);
     }
 
     // 특정 챌린지의 채팅방 내용을 조회하는 로직
@@ -58,19 +38,7 @@ public class InteractionService {
     // 챌린지에 후기 및 별점을 작성하는 로직
     public Review postReview(String challengeId, ReviewDto reviewDto) {
         Review review = interactionRepository.saveReview(challengeId, reviewDto);
-        sendReviewNotification(challengeId, reviewDto);
         return review;
-    }
-
-    // 챌린지 후기에 대한 알림을 보내는 로직
-    private void sendReviewNotification(String challengeId, ReviewDto reviewDto) {
-        InteractionNotificationMessage notificationMessage = InteractionNotificationMessage.builder()
-                .userIds(/* 챌린지 리더의 ID 목록 */)
-                .message("A new review and rating have been posted")
-                .chatMessage(reviewDto.getReviewText())
-                .challengeName(challengeId)
-                .build();
-        notificationService.sendNotification(notificationMessage);
     }
 
     // 특정 챌린지의 모든 후기 및 별점을 조회하는 로직
