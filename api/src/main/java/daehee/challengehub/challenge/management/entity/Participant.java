7a20ec03103043c3153915b253f6db8f34a33368
RYU-Dae-Hee <eogml0716@gmail.com>
2024-01-02 16:45:42 +0900

    ADD : Management 작업 - Challenge, Participant의 Entity와 Dto에서 필요한 필드 추가

--- api/src/main/java/daehee/challengehub/challenge/management/entity/Participant.java
+++ api/src/main/java/daehee/challengehub/challenge/management/entity/Participant.java
@@ -1,17 +1,21 @@
 package daehee.challengehub.challenge.management.entity;
 
+import lombok.Builder;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.Instant;
 
+@Builder
 @Document
 public class Participant {
     @Id
     private String id;
     private String challengeId;
-    private String userId; // 참가자 사용자 ID
-    private Instant joinDate; // 참가 날짜
-
-    // TODO: 챌린지에 참여한 사용자와 관련된 추가적인 정보들이 들어갈 수도 있을 거 같다.
+    private String userId;
+    private Instant joinDate;
+    private double achievementRate; // 참가자의 챌린지 달성율
+// TODO: 챌린지에 참여한 사용자와 관련된 추가적인 정보들이 들어갈 수도 있을 거 같다.
 }
+
+
